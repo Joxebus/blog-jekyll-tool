1252b24e5b95b425f78564dd519356059a3a9a88
Omar Bautista <joxebus@gmail.com>
2022-06-03 00:23:40 -0500

    Use picocli to parse the parameters and rename CommandLineService

--- src/main/groovy/io/github/joxebus/Application.groovy
+++ src/main/groovy/io/github/joxebus/Application.groovy
@@ -1,68 +1,88 @@
 package io.github.joxebus
 
-import groovy.transform.CompileStatic
-import groovy.transform.stc.POJO
+import groovy.cli.picocli.CliBuilder
+import groovy.cli.picocli.OptionAccessor
 import io.github.joxebus.conf.Configuration
-import io.github.joxebus.service.CommandService
+import io.github.joxebus.service.CommandLineService
 
-@POJO
-@CompileStatic
 class Application {
 
+    static CliBuilder buildCli(String[] args) {
+        CliBuilder cli = new CliBuilder(usage: "blog -[hscnld]")
+
+        cli.with {
+            h longOpt: 'help', 'Usage Information'
+            s longOpt: 'settings', 'Configure settings of the command line'
+            c longOpt: 'configure', 'Configure local user info'
+            n longOpt: 'new', args: '1', argName: 'title', 'Create new blog post with title'
+            l longOpt: 'list', 'List blog posts created'
+            d longOpt: 'delete', args: '1', argName: 'file', 'Deletes a blog post'
+        }
+
+        cli
+    }
+
     static void main(String[] args) {
-        CommandService commandService = new CommandService()
+        CliBuilder cli = buildCli(args)
+        CommandLineService commandService = new CommandLineService()
+        try {
 
-        if (!args || args.length > 2) {
-            println "Invalid arguments, please see the usage section: "
-            commandService.usage()
-            System.exit(0)
-        }
+            OptionAccessor options = cli.parse(args)
 
-        if(!Configuration.BLOG_FOLDER) {
-            println "You must setup your env var BLOG_FOLDER first"
-            System.exit(1)
-        }
-
-
-        if(!Configuration.homeFolder.exists()) {
-            println "Creating home folder [${Configuration.HOME_FOLDER}]"
-            Configuration.homeFolder.mkdirs()
-            commandService.configureSettings()
-        }
-
-        if (["-h", "--help"].contains(args[0])) {
-            commandService.usage()
-            System.exit(0)
-        }
-
-        if(["-s", "--settings"].contains(args[0])) {
-            commandService.configureSettings()
-            System.exit(0)
-        }
-
-        if(["-c", "--configure"].contains(args[0])) {
-            commandService.configure()
-            System.exit(0)
-        }
-
-        if(["-n", "--new"].contains(args[0])) {
-            if(args.length < 2) {
-                println "You must to specify a Title with the command -n or --new"
-                commandService.usage()
+            if(!Configuration.BLOG_FOLDER) {
+                println "You must setup your env var BLOG_FOLDER first"
                 System.exit(1)
             }
-            commandService.newPost(args[1])
-            System.exit(0)
+
+
+            if(!Configuration.homeFolder.exists()) {
+                println "Creating home folder [${Configuration.HOME_FOLDER}]"
+                Configuration.homeFolder.mkdirs()
+                commandService.configureSettings()
+            }
+
+            if(!options) {
+                System.exit(1)
+            }
+
+            if (options.h) {
+                cli.usage()
+                System.exit(0)
+            }
+
+            if(options.s) {
+                commandService.configureSettings()
+                System.exit(0)
+            }
+
+            if(options.c) {
+                commandService.configure()
+                System.exit(0)
+            }
+
+            if(options.n) {
+                if(args.length < 2) {
+                    println "You must to specify a Title with the command -n or --new"
+                    commandService.usage()
+                    System.exit(1)
+                }
+                commandService.newPost(options.n)
+                System.exit(0)
+            }
+
+            if(options.l) {
+                commandService.listPosts()
+                System.exit(0)
+            }
+
+            if(options.d) {
+                commandService.delete(options.d)
+                System.exit(0)
+            }
+        } catch(RuntimeException e) {
+            cli.usage()
         }
 
-        if(["-l", "--list"].contains(args[0])) {
-            commandService.listPosts()
-            System.exit(0)
-        }
 
-        if(["-d", "--delete"].contains(args[0])) {
-            commandService.delete(args[1])
-            System.exit(0)
-        }
     }
 }
--- src/main/groovy/io/github/joxebus/service/CommandLineService.groovy
+++ src/main/groovy/io/github/joxebus/service/CommandLineService.groovy
@@ -3,24 +3,10 @@ package io.github.joxebus.service
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class CommandService {
+class CommandLineService {
 
     ConfigurationService configurationService = new ConfigurationService()
     PostService postService = new PostService()
-
-    static final String USAGE = """
-usage: blog -[schnld]
- -s,--settings      Configure settings of the command line
- -c,--configure     Configure user info
- -h,--help          Usage Information
- -n,--new <Title>   Create new blog post with title
- -l,--list          List blog posts created
- -d,--delete <file> Deletes a blog post
-"""
-
-    void usage() {
-        println USAGE
-    }
 
     void configureSettings() {
         configurationService.configureSettings()
